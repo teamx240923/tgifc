c3cb0043a7ca76167f1934841ad99abc6e0784d8
ritika2407 <ritika.agarwal24@gmail.com>
2025-12-19 16:15:41 +0530

    changes for location

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -93,7 +93,7 @@ export default function HeroSection() {
                   <path d="M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7zm0 9.5A2.5 2.5 0 1 1 12 6a2.5 2.5 0 0 1 0 5.5z"/>
                 </svg>
                 <p className="leading-snug">
-                  Tau Devilal Stadium (near Medanta Hospital), Gurugram
+                  Tau Devilal Stadium, Gurugram
                 </p>
               </div>
 
